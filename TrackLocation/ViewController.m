00157211b412011f22a44ddfdc221a37fac319fc
ozx <442910407@qq.com>
2018-06-13 15:36:07 +0800

    添加提示框，2.socket通信解析

--- TrackLocation/ViewController.m
+++ TrackLocation/ViewController.m
@@ -17,6 +17,12 @@
 @property (nonatomic,assign) long readTag ;
 @property (nonatomic,strong) NSTimer * timer;
 @property (nonatomic,strong) GCDAsyncSocket * socket;
+
+@property (nonatomic,strong) MAMapView * mapView;
+@property (nonatomic,strong) MAPointAnnotation *pointAnnotation;
+
+@property (nonatomic,strong) UIView * hudView ;
+@property (nonatomic,strong) UILabel * tipLabel ;
 @end
 
 @implementation ViewController
@@ -28,20 +34,20 @@
     _socket = [[GCDAsyncSocket alloc] initWithDelegate:self delegateQueue:dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)];
     _writeTag  = 0;
     _readTag = 0;
-    MAMapView * mapView = [[MAMapView alloc] initWithFrame:self.view.frame];
+    _mapView = [[MAMapView alloc] initWithFrame:self.view.frame];
     MAUserLocation * location = [[MAUserLocation alloc] init];
     
-    mapView.showsUserLocation = YES;
-    mapView.userTrackingMode = MAUserTrackingModeFollow;
+    _mapView.showsUserLocation = YES;
+    _mapView.userTrackingMode = MAUserTrackingModeFollow;
     
-    MAPointAnnotation *pointAnnotation = [[MAPointAnnotation alloc] init];
-    pointAnnotation.coordinate = CLLocationCoordinate2DMake(23.149825 , 113.409873);
-    pointAnnotation.title = @"我的位置";
-    pointAnnotation.subtitle = @"ehang";
-    mapView.delegate = self;
-    [mapView addAnnotation:pointAnnotation];
+    _pointAnnotation = [[MAPointAnnotation alloc] init];
+    _pointAnnotation.coordinate = CLLocationCoordinate2DMake(23.149825 , 113.409873);
+    _pointAnnotation.title = @"我的位置";
+    _pointAnnotation.subtitle = @"ehang";
+    _mapView.delegate = self;
+    [_mapView addAnnotation:_pointAnnotation];
     
-    [self.view addSubview:mapView];
+    [self.view addSubview:_mapView];
     
     CGFloat screenWidth = [UIScreen mainScreen].bounds.size.width;
     CGFloat screenHeight = [UIScreen mainScreen].bounds.size.height;
@@ -50,7 +56,30 @@
     [connectBtn setTitleColor:[UIColor blueColor] forState:UIControlStateNormal];
     [connectBtn addTarget:self action:@selector(btnClick:) forControlEvents:UIControlEventTouchUpInside];
     
+    [_mapView setZoomLevel:16];
+    
     [self.view addSubview:connectBtn];
+    
+    _hudView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 280, 50)];
+    _hudView.backgroundColor = [UIColor clearColor];
+    _hudView.layer.cornerRadius = 5;
+    _hudView.layer.masksToBounds = true;
+    _hudView.center =self.view.center;
+    _hudView.hidden = true;
+    
+    UIView * backView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 280, 50)];
+    backView.backgroundColor = [UIColor blackColor];
+    backView.alpha = 0.7;
+    [_hudView addSubview:backView];
+    
+    _tipLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 280, 50)];
+    _tipLabel.textAlignment = NSTextAlignmentCenter;
+    _tipLabel.text = @"tip";
+    _tipLabel.font = [UIFont systemFontOfSize:16];
+    _tipLabel.textColor = [UIColor whiteColor];
+    [_hudView addSubview:_tipLabel];
+    
+    [self.view addSubview:_hudView];
 }
 
 - (void)btnClick:(UIButton *)btn{
@@ -89,27 +118,40 @@
 
 }
 
-
-
+- (void)showMessage:(NSString *)message{
+    typeof(self) weakSelf = self;
+    dispatch_async(dispatch_get_main_queue(), ^{
+        weakSelf.hudView.hidden  = false;
+        weakSelf.tipLabel.text = message;
+    });
+    
+    
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        [UIView animateWithDuration:1 animations:^{
+            weakSelf.hudView.hidden  = true;
+        }];
+    });
+}
 
 #pragma mark -socket的代理
 #pragma mark 连接成功
 -(void)socket:(GCDAsyncSocket *)sock didConnectToHost:(NSString *)host port:(uint16_t)port{
     NSLog(@"%s",__func__);
+    [self showMessage:@"连接服务器成功"];
     [_socket readDataWithTimeout:-1 tag:99];
 }
-
 
 #pragma mark 断开连接
 -(void)socketDidDisconnect:(GCDAsyncSocket *)sock withError:(NSError *)err{
     [_timer invalidate];
     if (err) {
+        [self showMessage:@"连接失败"];
         NSLog(@"连接失败");
     }else{
         NSLog(@"正常断开");
+        [self showMessage:@"正常断开"];
     }
 }
-
 
 #pragma mark 数据发送成功
 -(void)socket:(GCDAsyncSocket *)sock didWriteDataWithTag:(long)tag{
@@ -124,15 +166,24 @@
     NSString *receiverStr = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
     NSLog(@"%s %@",__func__,receiverStr);
     
+//    NSString * s = @"!23.147285970052085,113.4153502061632";
+    double lat = 0;
+    double lng = 0;
+    if ([receiverStr hasPrefix:@"!"]) {
+        NSString * l = [receiverStr substringFromIndex:1];
+        NSArray * latlng = [l componentsSeparatedByString:@","];
+        @try{
+            lat = [latlng[0] doubleValue];
+            lng = [latlng[1] doubleValue];
+        }@catch (NSException * ex){
+            NSLog(@"%@",ex);
+        }
+    }
+    
+    [_pointAnnotation setCoordinate:CLLocationCoordinate2DMake(lat , lng)];
+    [_mapView setCenterCoordinate:CLLocationCoordinate2DMake(lat , lng)];
     [_socket readDataWithTimeout:-1 tag:tag];
 }
-
-
-
-
-
-
-
 
 
 - (MAAnnotationView *)mapView:(MAMapView *)mapView viewForAnnotation:(id <MAAnnotation>)annotation
